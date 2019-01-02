8d2c3f84d43173104f5ac7a428ed1bbbfda1960e
Dean Babić <deanbabic@hotmail.com>
2019-01-02 16:59:07 +0000

    Set for now, built the scene and played with some settings

--- BuildingEscape/Source/BuildingEscape/MoveDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/MoveDoor.cpp
@@ -1,11 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReporter.h"
+#include "MoveDoor.h"
 #include "GameFramework/Actor.h"
-#include <string>
 
 // Sets default values for this component's properties
-UPositionReporter::UPositionReporter()
+UMoveDoor::UMoveDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -16,19 +15,20 @@ UPositionReporter::UPositionReporter()
 
 
 // Called when the game starts
-void UPositionReporter::BeginPlay()
+void UMoveDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	const auto ObjectName = GetOwner()->GetName();
-	const auto ObjectPosition = GetOwner()->GetTransform().GetLocation().ToString();
-	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPosition);
+	auto Owner = GetOwner();
+	const auto NewRotation = FRotator(0.0f, 70.0f, 0.0f);
+	Owner->SetActorRotation(NewRotation);
+
 	
 }
 
 
 // Called every frame
-void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UMoveDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 

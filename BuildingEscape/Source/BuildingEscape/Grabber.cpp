c505c521bd36cd70b20f90f796a3186d241962f0
Dean Babić <deanbabic@hotmail.com>
2019-01-04 16:07:54 +0000

    Implemented Grabber method

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -1,10 +1,8 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "Grabber.h"
 #include "Engine/World.h"
 #include "GameFramework/PlayerController.h"
 #include "DrawDebugHelpers.h"
-
+#include "Components/PrimitiveComponent.h"
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -12,20 +10,16 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
-
 
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FindPhysicsHandleComponent();
+	SetupInputComponent();
 }
-
 
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
@@ -35,11 +29,92 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	FVector PlayerLocation;
 	FRotator PlayerRotator;
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PlayerLocation, PlayerRotator);
-	/*UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"), 
-		*PlayerLocation.ToString(), 
-		*PlayerRotator.ToString()
-	);*/
-	FVector LineTraceEnd = PlayerLocation + PlayerRotator.Vector() * HandReach;
-	DrawDebugLine(GetWorld(), PlayerLocation, LineTraceEnd, FColor(200,0,200), false, 0, 0, 2);
+	const FVector LineTraceEnd = PlayerLocation + PlayerRotator.Vector() * HandReach;
+
+	if(PhysicsHandle->GrabbedComponent)
+	{
+		PhysicsHandle->SetTargetLocation(LineTraceEnd);
+	}
 }
 
+/// Look for attached Physics handle
+void UGrabber::FindPhysicsHandleComponent()
+{
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle)
+	{
+		// Physics handle is found
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component!"), *GetOwner()->GetName())
+	}
+}
+
+/// Look for attached Physics handle
+void UGrabber::SetupInputComponent()
+{
+	InputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (InputComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Input component found"))
+		
+		/// Bind the input axis
+		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing input component!"), *GetOwner()->GetName())
+	}
+}
+
+// Ray-cast and grab what is within reach
+void UGrabber::Grab()
+{
+	auto HitResult = GetFirstPhysicsBodyInReach();
+	const auto ComponentToGrab = HitResult.GetComponent();
+	const auto ActorHit = HitResult.GetActor();
+	if(ActorHit)
+	{
+		PhysicsHandle->GrabComponentAtLocationWithRotation(
+			ComponentToGrab,
+			NAME_None,
+			HitResult.ImpactPoint,
+			ComponentToGrab->GetComponentRotation()
+			);
+	}
+}
+
+void UGrabber::Release()
+{
+	PhysicsHandle->ReleaseComponent();
+}
+
+FHitResult UGrabber::GetFirstPhysicsBodyInReach() const
+{
+	FVector PlayerLocation;
+	FRotator PlayerRotator;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PlayerLocation, PlayerRotator);
+	const FVector LineTraceEnd = PlayerLocation + PlayerRotator.Vector() * HandReach;
+
+	/*
+	 * //Draw a line in the world to visualize
+	 * DrawDebugLine(GetWorld(), PlayerLocation, LineTraceEnd, FColor(200, 0, 200), false, 0, 0, 2);
+	 */
+
+	 /// Setup query parameters
+	const FCollisionQueryParams CollisionQueryParams(FName(TEXT("")), false, GetOwner());
+	FHitResult Hit;
+
+	/// Line-trace (AKA ray-cast) out to reach distance
+	GetWorld()->LineTraceSingleByObjectType(
+		Hit,
+		PlayerLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		CollisionQueryParams
+	);
+	return Hit;
+}
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
 #include "Grabber.generated.h"
 
 
@@ -15,6 +17,7 @@ class BUILDINGESCAPE_API UGrabber : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UGrabber();
+	
 
 protected:
 	// Called when the game starts
@@ -27,5 +30,19 @@ public:
 private:
 	UPROPERTY(EditAnywhere)
 		float HandReach = 100.0f;
-		
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponent = nullptr;
+
+	// Find (assumed) attached physics handle
+	void FindPhysicsHandleComponent();
+	// Setup (assumed) attached input component
+	void SetupInputComponent();
+	// Ray-cast and grab what is within reach
+	void Grab();
+	// Called when grab is released
+	void Release();
+	// Return hit for first physics body in reach
+	FHitResult GetFirstPhysicsBodyInReach() const;
+
 };
